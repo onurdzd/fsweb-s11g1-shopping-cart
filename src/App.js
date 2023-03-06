d9dc7fc8c182ff2829ac36c5a958dd684da8eaa0
onurd <106928484+onurdzd@users.noreply.github.com>
2023-03-06 21:16:04 +0300

    locale data gönderiyor,localdan silme yapıyor sayfa yenileyince hata veriyor

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Route } from "react-router-dom";
 import { data } from "./data";
 import { CartContext,ProductContext } from "./contexts";
@@ -10,16 +10,34 @@ import ShoppingCart from "./components/ShoppingCart";
 
 function App() {
   const [products, setProducts] = useState(data);
+  const [localCart,setLocalCart]=useState(localStorage.getItem("sepetItem") ? localStorage.getItem("sepetItem") : [])
   const [cart, setCart] = useState([]);
 
   const addItem = (item) => {
     // verilen itemi sepete ekleyin
-    setCart([...cart,item])
+    setCart([...cart,item]);
+    localeEkle(item);
   };
+  
+  const localeEkle=(item)=>{
+    localStorage.setItem("sepetItem",JSON.stringify([...localCart, item]))
+    setLocalCart([...localCart,item])
+  }
 
   const removeItem=(item)=>{
-    setCart(cart.filter(elem=> elem.id!==item.id))
+    localdenSil(item)
   }
+
+  const localdenSil=(item)=>{
+    localStorage.setItem("sepetItem",JSON.stringify((cart.filter(elem=> elem.id!==item.id))))
+    setLocalCart(cart.filter(elem=> elem.id!==item.id))
+  }
+
+  useEffect(()=>{
+    setCart(localCart)
+  },[localCart])
+
+  console.log(cart)
 
   return (
     <div className="App">
